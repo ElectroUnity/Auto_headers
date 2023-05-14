42796b0653f56efb7a9cfae88f4235cf33da2bee
ElectroUnity <121370149+ElectroUnity@users.noreply.github.com>
2023-05-14 11:12:14 +0800

    Update headers.py

--- headers.py
+++ headers.py
@@ -6,9 +6,6 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 
 def create_headers():
-    """
-    :return:
-    """
     ua_last = ua.random
     headers = {'User-Agent': ua_last}
     return headers # 返回一个字典
